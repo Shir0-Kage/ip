9c5340ff0f8660de326cc63abab1e534108d3688
Shir0-Kage <zhouzehao1783174@gmail.com>
2026-08-23 00:41:39 +0800

    Echo

--- src/main/java/Benjamin.java
+++ src/main/java/Benjamin.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Benjamin {
     public static void main(String[] args) {
         String banner = " ____             _                 _\n"
@@ -8,8 +10,28 @@ public class Benjamin {
                 + "                |__/\n";
         String greeting = "Hello! I'm Benjamin.\n"
                 + "What can I do for you?\n";
+
+        String divider = "————————————————————————————————————————————————————————————\n";
+        
+
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println(banner + greeting + "\n");
+
+        String input = scanner.nextLine(); 
+
+        while (input != "bye") {
+
+            System.out.println(divider + input + "\n" + divider);
+
+            input = scanner.nextLine(); 
+        }
+
         String farewell = "Bye. Hope to see you again soon!";
 
-        System.out.println(banner + greeting + "\n" + farewell);
+        System.out.println(farewell);
+
+        scanner.close(); 
+
     }
 }
