d158b1c7334f9f3dcf350a90b3fd55ce448d5a5e
Shir0-Kage <zhouzehao1783174@gmail.com>
2026-08-23 00:24:25 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Benjamin.java
@@ -0,0 +1,15 @@
+public class Benjamin {
+    public static void main(String[] args) {
+        String banner = " ____             _                 _\n"
+                + "| __ )  ___ _ __ (_) __ _ _ __ ___ (_)_ __\n"
+                + "|  _ \\ / _ \\ '_ \\| |/ _` | '_ ` _ \\| | '_ \\\n"
+                + "| |_) |  __/ | | | | (_| | | | | | | | | | |\n"
+                + "|____/ \\___|_| |_|/ |\\__,_|_| |_| |_|_|_| |_|\n"
+                + "                |__/\n";
+        String greeting = "Hello! I'm Benjamin.\n"
+                + "What can I do for you?\n";
+        String farewell = "Bye. Hope to see you again soon!";
+
+        System.out.println(banner + greeting + "\n" + farewell);
+    }
+}
